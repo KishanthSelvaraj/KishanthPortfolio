b91e2441cfb639d1e241b25039100310153521c7
Kishanth S <154578200+KishanthSelvaraj@users.noreply.github.com>
2024-10-18 20:18:42 +0530

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+import { IoLogoVercel } from "react-icons/io5";
 
 const socials = [
   { icon: <FaGithub />, path: "https://github.com/KishanthSelvaraj" },
   { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/kishanth-selvaraj-a71698259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
-  // { icon: <FaYoutube />, path: "" },
+  { icon: <IoLogoVercel />, path: "https://vercel.com/kishanthselvarajs-projects" },
   // { icon: <FaTwitter />, path: "" },
 ];
 const Social = ({ containerStyles, iconStyle }) => {
